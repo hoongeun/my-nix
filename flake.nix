37a39d139cbb0f3b8cdd46bef14e894916d1a1b7
hoongeun <me@hoongeun.com>
2024-03-03 12:58:39 +0000

    feat(flake): update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,96 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, rust-overlay }:
+  let
+    systemSettings = {
+      system = "x86_64-linux";
+      hostname = "hoongeun";
+      profile = "wsl";
+      timezone = "Asia/Seoul";
+    };
+
+    userSettings = rec {
+      username = "hoongeun";
+      name = "Hoongeun";
+      email = "me@hoongeun.com";
+      dotfileDirs = "~/.my-nix";
+      editor = "hx";
+    };
+
+    pkgs = import nixpkgs-stable {
+      system = systemSettings.system;
+      config = {
+        allowUnfree = true;
+        allowUnfreePredicate = (_: true);
+        overlays = [ rust-overlay.overlays.default ];
+      };
+    };
+
+    lib = nixpkgs.lib;
+
+    supportedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+    ];
+
+    forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+
+    nixpkgsFor = forAllSystems (system:
+      import inputs.nixpkgs { inherit system; });
+
+  in
+  {
+    homeConfigurations = {
+      user = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ (./. + "/profiles"+("/"+systemSettings.profile)+"/home.nix") ];
+        extraSpecialArgs = {
+          inherit pkgs;
+          inherit systemSettings;
+          inherit userSettings;
+        };
+      };
+    };
+
+    nixosConfigurations = {
+      system = lib.nixosSystem {
+        system = systemSettings.system;
+        modules = [ (./. + "/profiles"+("/"+systemSettings.profiel)+"configurations.nix") ];
+        specialArgs = {
+          inherit pkgs;
+          inherit systemSettings;
+          inherit userSettings;
+        };
+      };
+    };
+
+    packages = forAllSystems (system:
+      let pkgs = nixpkgsFor.${system}; in
+      {
+        defualt = self.pakcages.${system}.install;
+
+        install = pkgs.writeScriptBin "install" ./install.sh;
+      });
+
+    apps = forAllSystems (system: {
+      default = self.apps.${system}.install;
+
+      install = {
+        type = "app";
+        program = "${self.packages.${system}.install}/bin/install";
+      };
+    });
   };
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 }
