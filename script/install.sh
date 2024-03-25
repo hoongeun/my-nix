5ee8f0a0576349c23ac2623111c3b66a03fa9613
Hoongeun <me@hoongeun.com>
2024-03-25 23:28:29 +0900

    feat(script): add home-manager install command

--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 sudo nixos-rebuild switch --flake .#system --impure
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake .#user;
